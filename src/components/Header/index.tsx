f010a3299b9b5783ad2a389b7c5800a155d2e077
Eduardo Sousa <eduardobrunoit@gmail.com>
2023-09-14 19:37:26 -0400

    feat: add svg to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,10 +2,16 @@ import { Text, View } from 'react-native';
 
 import { styles } from './styles';
 
+import Rocket from '../../../assets/header/rocket.svg';
+import To from '../../../assets/header/to.svg';
+import Do from '../../../assets/header/do.svg';
+
 export default function Header() {
   return (
     <View style={styles.container}>
-      <Text>todo</Text>
+      <Rocket width={50} height={50} />
+      <To width={45} height={50} />
+      <Do width={55} height={50} />
     </View>
   );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -4,6 +4,9 @@ export const styles = StyleSheet.create({
     container: {
         width: '100%',
         height: 185,
-        backgroundColor: '#F12123' //#0D0D0D
+        backgroundColor: '#0D0D0D',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
     }
 });
