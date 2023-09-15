eef6e593750fde3abd0f968513b0a1a28af8701f
Eduardo Sousa <eduardobrunoit@gmail.com>
2023-09-14 20:04:12 -0400

    fix: change placeholder to english

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -7,7 +7,7 @@ export default function Form() {
   return (
     <View style={styles.form}>
       <TextInput
-        placeholder="Adicione uma nova tarefa"
+        placeholder="Add a new task"
         placeholderTextColor="#808080"
         style={styles.input}
       />
