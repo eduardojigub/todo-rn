a8220a883352fe73a4416dcf0c6377708bb419c0
Eduardo Sousa <eduardobrunoit@gmail.com>
2023-09-15 09:29:58 -0400

    feat: create tasker counter

--- src/components/TasksController/index.tsx
+++ src/components/TasksController/index.tsx
@@ -1,13 +1,17 @@
-import { View, Text, TextInput } from 'react-native';
+import { View, Text } from 'react-native';
 import { styles } from './styles';
+import { useTask } from '../../context/TaskContext'; // Import the context
 
 export default function TasksController() {
+  const { taskList } = useTask();
+
+  const createdTasksCount = taskList.length;
   return (
     <View style={styles.containerTaskController}>
       <View style={styles.counterCointaner}>
         <Text style={styles.createdTasksText}>Created</Text>
         <View style={styles.counterBackground}>
-          <Text style={styles.counter}>0</Text>
+          <Text style={styles.counter}>{createdTasksCount}</Text>
         </View>
       </View>
       <View style={styles.counterCointaner}>
